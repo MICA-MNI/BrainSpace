626f587dcf272ec9d0fa22fab1238dd12396066e
oualid <bmoc07@gmail.com>
2019-09-05 02:30:30 -0400

    Update examples

--- brainspace/examples/plot_tutorial2.py
+++ brainspace/examples/plot_tutorial2.py
@@ -4,7 +4,8 @@ Tutorial 2: Customizing and aligning gradients
 In this tutorial you’ll learn about the methods available within the
 GradientMaps class. The flexible usage of this class allows for the
 customization of gradient computation with different kernels and dimensionality
-reductions, as well as aligning gradients from different datasets.
+reductions, as well as aligning gradients from different datasets. This
+tutorial will only show you how to apply these techniques.
 """
 
 
@@ -14,11 +15,11 @@ reductions, as well as aligning gradients from different datasets.
 import warnings
 warnings.simplefilter('ignore')
 
-from brainspace.datasets import load_group_hcp, load_parcellation, load_conte69
+from brainspace.datasets import load_group_fc, load_parcellation, load_conte69
 
 # First load mean connectivity matrix and Schaefer parcellation
-conn_matrix = load_group_hcp('schaefer', n_parcels=400)
-labeling = load_parcellation('schaefer', n_parcels=400)
+conn_matrix = load_group_fc('schaefer', scale=400)
+labeling = load_parcellation('schaefer', scale=400, join=True)
 mask = labeling != 0
 
 # and load the conte69 hemisphere surfaces
@@ -42,12 +43,13 @@ for i, k in enumerate(kernels):
     gm = GradientMaps(kernel=k, approach='dm', random_state=0)
     gm.fit(conn_matrix)
 
-    gradients_kernel[i] = map_to_labels(gm.gradients_[:, i], labeling,
-                                        mask=mask, fill=np.nan)
+    gradients_kernel[i] = map_to_labels(gm.gradients_[:, i], labeling, mask=mask,
+                                        fill=np.nan)
 
 
-plot_hemispheres(surf_lh, surf_rh, array_name=gradients_kernel,
-                 size=(800, 450), cmap_name='viridis')
+label_text = ['Pearson', 'Spearman', 'Normalized\nAngle']
+plot_hemispheres(surf_lh, surf_rh, array_name=gradients_kernel, size=(800, 450),
+                 cmap='viridis', color_bar=True, label_text=label_text)
 
 
 ###############################################################################
@@ -57,6 +59,7 @@ plot_hemispheres(surf_lh, surf_rh, array_name=gradients_kernel,
 # Similar to using different kernels, we can also use different dimensionality
 # reduction techniques.
 
+# PCA, Laplacian eigenmaps and diffusion mapping
 embeddings = ['pca', 'le', 'dm']
 
 gradients_embedding = [None] * len(embeddings)
@@ -64,36 +67,28 @@ for i, emb in enumerate(embeddings):
     gm = GradientMaps(kernel='normalized_angle', approach=emb, random_state=0)
     gm.fit(conn_matrix)
 
-    gradients_embedding[i] = map_to_labels(gm.gradients_[:, i], labeling,
-                                           mask=mask, fill=np.nan)
+    gradients_embedding[i] = map_to_labels(gm.gradients_[:, 0], labeling, mask=mask,
+                                           fill=np.nan)
 
 
-plot_hemispheres(surf_lh, surf_rh, array_name=gradients_embedding,
-                 size=(800, 452), cmap_name='viridis')
+label_text = ['PCA', 'LE', 'DM']
+plot_hemispheres(surf_lh, surf_rh, array_name=gradients_embedding, size=(800, 450),
+                 cmap='viridis', color_bar=True, label_text=label_text)
 
 
 ###############################################################################
-# Here we do see some substantial differences: PCA appears to find a slightly
-# different axis, with the somatomotor in the middle between default mode and
-# visual, whereas LE and DM both find the canonical first gradient but their
-# signs are flipped. Fortunately, the sign of gradients is arbitrary, so we
-# could simply multiply either the LM and DM gradient by -1 to make them more
-# comparable.
-#
 # A more principled way of increasing comparability across gradients are
 # alignment techniques. BrainSpace provides two alignment techniques:
 # Procrustes analysis, and joint alignment. For this example we will load
 # functional connectivity data of a second subject group and align it with the
 # first group.
 
-from brainspace.datasets import load_holdout_hcp
-
-conn_matrix2 = load_holdout_hcp('schaefer', 400)
+conn_matrix2 = load_group_fc('schaefer', scale=400, group='holdout')
 gp = GradientMaps(kernel='normalized_angle', alignment='procrustes')
 gj = GradientMaps(kernel='normalized_angle', alignment='joint')
 
-gp.fit([conn_matrix2, conn_matrix])
-gj.fit([conn_matrix2, conn_matrix])
+gp.fit([conn_matrix, conn_matrix2])
+gj.fit([conn_matrix, conn_matrix2])
 
 
 ###############################################################################
@@ -101,44 +96,68 @@ gj.fit([conn_matrix2, conn_matrix])
 # aligned data. Let’s plot them, but in separate figures to keep things
 # organized.
 
-# First gradient from holdout and original data
+# First gradient from original and holdout data, without alignment
 gradients_unaligned = [None] * 2
 for i in range(2):
     gradients_unaligned[i] = map_to_labels(gp.gradients_[i][:, 0], labeling,
                                            mask=mask, fill=np.nan)
 
-plot_hemispheres(surf_lh, surf_rh, array_name=gradients_unaligned,
-                 size=(800, 300), cmap_name='viridis')
+label_text = ['Unaligned Group 1', 'Unaligned Group 2']
+plot_hemispheres(surf_lh, surf_rh, array_name=gradients_unaligned, size=(800, 300),
+                 cmap='viridis', color_bar=True, label_text=label_text)
 
 
 ###############################################################################
 
+# With procrustes alignment
 gradients_procrustes = [None] * 2
 for i in range(2):
-    gradients_procrustes[i] = map_to_labels(gp.aligned_[i][:, 0], labeling,
-                                            mask=mask, fill=np.nan)
+    gradients_procrustes[i] = map_to_labels(gp.aligned_[i][:, 0], labeling, mask=mask,
+                                            fill=np.nan)
 
-plot_hemispheres(surf_lh, surf_rh, array_name=gradients_procrustes,
-                 size=(800, 300), cmap_name='viridis')
+label_text = ['Procrustes Group 1', 'Procrustes Group 2']
+plot_hemispheres(surf_lh, surf_rh, array_name=gradients_procrustes, size=(800, 300),
+                 cmap='viridis', color_bar=True, label_text=label_text)
 
 
 ###############################################################################
 
+# With joint alignment
 gradients_joint = [None] * 2
 for i in range(2):
-    gradients_joint[i] = map_to_labels(gj.aligned_[i][:, 0], labeling,
-                                       mask=mask, fill=np.nan)
+    gradients_joint[i] = map_to_labels(gj.aligned_[i][:, 0], labeling, mask=mask,
+                                       fill=np.nan)
 
-plot_hemispheres(surf_lh, surf_rh, array_name=gradients_joint,
-                 size=(800, 300), cmap_name='viridis')
+label_text = ['Joint Group 1', 'Joint Group 2']
+plot_hemispheres(surf_lh, surf_rh, array_name=gradients_joint, size=(800, 300),
+                 cmap='viridis', color_bar=True, label_text=label_text)
 
 
 ###############################################################################
-# Before gradient alignment, the first gradient is reversed, but both
-# alignments resolve this issue. If the input data was less similar, alignments
-# may also resolve changes in the order of the gradients. However, you should
-# always inspect the output of an alignment; if the input data are sufficiently
-# dissimilar then the alignment may produce odd results.
+# Although in this example, we don't see any big differences, if the input data
+# was less similar, alignments may also resolve changes in the order of the
+# gradients. However, you should always inspect the output of an alignment;
+# if the input data are sufficiently dissimilar then the alignment may produce
+# odd results.
+#
+#
+# In some instances, you may want to align gradients to an out-of-sample
+# gradient, for example when aligning individuals to a hold-out group gradient.
+# When performing a Procrustes alignemnt, a 'reference' can be specified.
+# The first alignment iteration will then be to the reference. For purposes of
+# this example, we will use the gradient of the hold-out group as the
+# reference.
+
+gref = GradientMaps(kernel='normalized_angle', approach='le')
+gref.fit(conn_matrix2)
+
+galign = GradientMaps(kernel='normalized_angle', approach='le', alignment='procrustes')
+galign.fit(conn_matrix, reference=gref.gradients_)
+
+
+###############################################################################
+# The gradients in `galign.aligned_` are now aligned to the reference
+# gradients.
 #
 # That concludes the second tutorial. In the third tutorial we will consider
 # null hypothesis testing of comparisons between gradients and other markers.
