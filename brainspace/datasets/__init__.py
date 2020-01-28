c8a2348f88f0709db4957b9452e34cfc280a2e34
oualid <bmoc07@gmail.com>
2020-01-28 15:55:46 -0500

    Add load_group_mpc and corresponding data

--- brainspace/datasets/__init__.py
+++ brainspace/datasets/__init__.py
@@ -1,9 +1,10 @@
-from .base import (load_conte69, load_mask, load_group_fc, load_parcellation,
-                   load_gradient, load_marker)
+from .base import (load_conte69, load_mask, load_group_fc, load_group_mpc,
+                   load_parcellation, load_gradient, load_marker)
 
 __all__ = ['load_conte69',
            'load_mask',
            'load_group_fc',
+           'load_group_mpc',
            'load_gradient',
            'load_marker',
            'load_parcellation']
