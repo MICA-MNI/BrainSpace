a6c3c3e509c568ca7e51dc947e3b3223973a3abb
Oualid <oualid.benkarim@mcgill.ca>
2021-06-01 16:23:20 -0400

    Update test_plotting.py

--- brainspace/tests/test_plotting.py
+++ brainspace/tests/test_plotting.py
@@ -90,7 +90,7 @@ def test_plotter_ipython():
 def test_plotter_numpy():
     s = to_data(vtk.vtkSphereSource())
 
-#     p = Plotter(offscreen=True)
+    p = Plotter(offscreen=True)
 #     ren0 = p.AddRenderer(row=0, col=0)
 #     ac0 = ren0.AddActor()
 #     ac0.SetMapper(inputdata=s)
