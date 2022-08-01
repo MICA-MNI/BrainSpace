4f85526256ce60166ac8cf86d8fd500a325b8e85
oualidbenkarim <Linkinp@rk85>
2022-08-01 12:54:23 -0400

    Add generator version of SpinPermutations.randomize

--- brainspace/null_models/spin.py
+++ brainspace/null_models/spin.py
@@ -289,10 +289,14 @@ class SpinPermutations(BaseEstimator):
         Returns
         -------
         rand_lh : ndarray, shape = (n_rep, n_lh, n_feat)
-            Permutations of `x_rh`. If ``n_feat == 1``, shape = (n_rep, n_lh).
+            Permutations of `x_lh`. If ``n_feat == 1``, shape = (n_rep, n_lh).
         rand_lh : ndarray, shape = (n_rep, n_rh, n_feat)
             Permutations of `x_rh`. If ``n_feat == 1``, shape = (n_rep, n_rh).
             None if `x_rh` is None. Only if `spin_rh_` is not None.
+
+        See Also
+        --------
+        :func:`.SpinPermutations.randomize_gen`
 
         """
 
@@ -305,3 +309,38 @@ class SpinPermutations(BaseEstimator):
             rand_rh = x_rh[self.spin_rh_]
 
         return rand_lh, rand_rh
+
+    def randomize_gen(self, x_lh, x_rh=None):
+        """ Generate random samples from `x_lh` and `x_rh`.
+
+        This is the generator version of :func:`.SpinPermutations.randomize`.
+
+        Parameters
+        ----------
+        x_lh : ndarray, shape = (n_lh,) or (n_lh, n_feat)
+            Array of variables arranged in columns, where `n_feat` is the
+            number of variables.
+        x_rh : ndarray, shape = (n_rh,) or (n_rh, n_feat), optional
+            Array of variables arranged in columns for the right hemisphere.
+            Default is None.
+
+        Yields
+        -------
+        rand_lh : ndarray, shape = (n_lh, n_feat)
+            Permutation of `x_lh`. If ``n_feat == 1``, shape = (n_lh,).
+        rand_lh : ndarray, shape = (n_rh, n_feat)
+            Permutation of `x_rh`. If ``n_feat == 1``, shape = (n_rh,).
+            None if `x_rh` is None. Only if `spin_rh_` is not None.
+
+        See Also
+        --------
+        :func:`.SpinPermutations.randomize`
+        """
+
+        for i in range(self.n_rep):
+            rand_lh = x_lh[self.spin_lh_[i]]
+            if self.spin_rh_ is not None and x_rh is not None:
+                rand_rh = x_rh[self.spin_rh_[i]]
+                yield rand_lh, rand_rh
+            else:
+                yield rand_lh
